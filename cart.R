08664618e8f236599bfc3f549919b4d42708aeed
romele-stefano <romele.stefano@gmail.com>
2019-09-19 18:39:04 +0200

    first commit

---
+++ cart.R
@@ -0,0 +1,241 @@
+#################################
+#                               #
+# DATA EXTRACTION: 18/09/2019   #
+# START DATA POINT: 03/06/2019  #
+# FINAL DATA POINT: 17/09/2019  #
+#                               #
+#################################
+
+
+setwd("C:/Users/ROMEST/Downloads/Performance/")
+
+options(scipen=999)
+
+# read excel file
+library(readxl)
+# split column from excel import
+library("tidyr")
+
+
+################################
+#                              #
+#     PERFORMANCE CARRELLO     #
+#                              #
+################################
+
+
+cart_performance_excel <- read_excel("cart_performance_excel.xlsx")
+data <- as.data.frame(cart_performance_excel)
+
+# separate column
+data <- separate(data, Time, into = c("empty", "time"), sep = " (?=[^ ]+$)")
+data <- data[,-2]
+names(data)[4] <- "avg_response"
+names(data)[1] <- "date"
+# add column for unique ID
+data$id <- seq.int(nrow(data))
+# remove comma from number
+data$avg_response <- as.numeric(gsub(",","", data$avg_response))
+# add column for month extraction
+data$new <- data$date
+# split new column for month extraction
+data <- separate(data, new, into = c("year", "month", "day"), sep = "-")
+
+# check for visible outliers
+plot(data$date, data$avg_response, main = "Performance Carrello",
+     xlab = "Data", ylab = "Tempo risposta medio (3 ore), in millisecondi")
+
+# extract outliers rows for investivation
+outliers <- data[which(data$avg_response > 400000),]
+final_data <- data[-which(data$avg_response > 400000), ]
+
+plot(final_data$date, final_data$avg_response, type = "l", main = "Performance Carrello",
+     xlab = "Data", ylab = "Tempo risposta medio (3 ore), in millisecondi")
+
+# find number of instances where server response > 10 sec
+nrow(final_data[which(final_data$avg_response > 10000), ])
+# extract rows
+g10 <- final_data[which(final_data$avg_response > 10000), ]
+# show distribution
+table(g10$time)
+# find number of instances where server response > 5 sec
+nrow(final_data[which(final_data$avg_response > 5000), ])
+# extract rows
+g5 <- final_data[which(final_data$avg_response > 5000), ]
+# show distribution
+table(g5$time)
+# find number of instances where server response > 2.5 sec
+nrow(final_data[which(final_data$avg_response > 2500), ])
+# extract rows
+g2.5 <- final_data[which(final_data$avg_response > 2500), ]
+# show distribution
+table(g2.5$time)
+# find number of instances where server response > 1.5 sec
+nrow(final_data[which(final_data$avg_response > 1500), ])
+# extract rows
+g1.5 <- final_data[which(final_data$avg_response > 1500), ]
+# show distribution
+table(g1.5$time)
+
+
+# create subset data for each month
+june <- subset(final_data, month == "06")
+july <- subset(final_data, month == "07")
+august <- subset(final_data, month == "08")
+september <- subset(final_data, month == "09")
+
+
+# JUNE
+plot(june$id, june$avg_response, type = "l", main = "Giugno - performance carrello",
+     xlab = "ID, 8 osservazioni per giorno (una osservazione ogni 3 ore)", ylab = "Tempo risposta medio, in millisecondi")
+# add mean
+mx <- mean(june$avg_response)
+abline(h = mx, col = "blue", lwd = 2)
+# text(locator(1), 'Tempo medio risposta: 3.301 secondi')
+
+# JULY
+plot(july$id, july$avg_response, type = "l", main = "Luglio - performance carrello",
+     xlab = "ID, 8 osservazioni per giorno (una osservazione ogni 3 ore)", ylab = "Tempo risposta medio, in millisecondi")
+# add mean
+mx <- mean(july$avg_response)
+abline(h = mx, col = "blue", lwd = 2)
+# text(locator(1), 'Tempo medio risposta: 2.584 secondi')
+
+# AUGUST
+plot(august$id, august$avg_response, type = "l", main = "Agosto - performance carrello",
+     xlab = "ID, 8 osservazioni per giorno (una osservazione ogni 3 ore)", ylab = "Tempo risposta medio, in millisecondi")
+# add mean
+mx <- mean(august$avg_response)
+abline(h = mx, col = "blue", lwd = 2)
+# text(locator(1), 'Tempo medio risposta: 1.527 secondi')
+
+# SEPTEMBER
+plot(september$id, september$avg_response, type = "l", main = "Settembre - performance carrello",
+     xlab = "ID, 8 osservazioni per giorno (una osservazione ogni 3 ore)", ylab = "Tempo risposta medio, in millisecondi")
+# add mean
+mx <- mean(september$avg_response)
+abline(h = mx, col = "blue", lwd = 2)
+# text(locator(1), 'Tempo medio risposta: 1.541 secondi')
+
+
+
+
+# check situation before IT fix (june/july) vs after IT fix (august/september)
+# create data frame
+before <- rbind(june, july)
+after <- rbind(august, september)
+
+# TIME OF THE DAY
+# 10 SECONDS
+# BEFORE
+# extract rows where avg response > 10 sec
+b10s <- before[which(before$avg_response > 10000), ]
+# show distribution
+table(b10s$time)
+# AFTER
+# extract rows where avg response > 10 sec
+a10s <- after[which(after$avg_response > 10000), ]
+# show distribution
+table(a10s$time)
+
+# 5 SECONDS
+# BEFORE
+# extract rows where avg response > 5 sec
+b5s <- before[which(before$avg_response > 5000), ]
+# show distribution
+table(b5s$time)
+# AFTER
+# extract rows where avg response > 5 sec
+a5s <- after[which(after$avg_response > 5000), ]
+# show distribution
+table(a5s$time)
+
+# 2.5 SECONDS
+# BEFORE
+# extract rows where avg response > 2.5 sec
+b2.5s <- before[which(before$avg_response > 2500), ]
+# show distribution
+table(b2.5s$time)
+nrow(before[which(before$avg_response > 2500), ])
+# AFTER
+# extract rows where avg response > 2.5 sec
+a2.5s <- after[which(after$avg_response > 2500), ]
+# show distribution
+table(a2.5s$time)
+nrow(after[which(after$avg_response > 2500), ])
+
+# 1.5 SECONDS
+# BEFORE
+# extract rows where avg response > 1.5 sec
+b1.5s <- before[which(before$avg_response > 1500), ]
+# show distribution
+table(b1.5s$time)
+nrow(before[which(before$avg_response > 1500), ])
+# AFTER
+# extract rows where avg response > 1.5 sec
+a1.5s <- after[which(after$avg_response > 1500), ]
+# show distribution
+table(a1.5s$time)
+nrow(after[which(after$avg_response > 1500), ])
+
+
+
+
+
+# DATE
+# 10 SECONDS
+# BEFORE
+# extract rows where avg response > 10 sec
+b10s <- before[which(before$avg_response > 10000), ]
+# show distribution
+table(b10s$date)
+# AFTER
+# extract rows where avg response > 10 sec
+a10s <- after[which(after$avg_response > 10000), ]
+# show distribution
+table(a10s$date)
+
+# 5 SECONDS
+# BEFORE
+# extract rows where avg response > 5 sec
+b5s <- before[which(before$avg_response > 5000), ]
+# show distribution
+table(b5s$date)
+# AFTER
+# extract rows where avg response > 5 sec
+a5s <- after[which(after$avg_response > 5000), ]
+# show distribution
+table(a5s$date)
+
+# 2.5 SECONDS
+# BEFORE
+# extract rows where avg response > 2.5 sec
+b2.5s <- before[which(before$avg_response > 2500), ]
+# show distribution
+table(b2.5s$date)
+nrow(before[which(before$avg_response > 2500), ])
+# AFTER
+# extract rows where avg response > 2.5 sec
+a2.5s <- after[which(after$avg_response > 2500), ]
+# show distribution
+table(a2.5s$date)
+nrow(after[which(after$avg_response > 2500), ])
+
+# 1.5 SECONDS
+# BEFORE
+# extract rows where avg response > 1.5 sec
+b1.5s <- before[which(before$avg_response > 1500), ]
+# show distribution
+table(b1.5s$date)
+nrow(before[which(before$avg_response > 1500), ])
+# AFTER
+# extract rows where avg response > 1.5 sec
+a1.5s <- after[which(after$avg_response > 1500), ]
+# show distribution
+table(a1.5s$date)
+nrow(after[which(after$avg_response > 1500), ])
+
+
+
+
+
---
+++ ricerca_nodo1.R
@@ -0,0 +1,135 @@
+#################################
+#                               #
+# DATA EXTRACTION: 18/09/2019   #
+# START DATA POINT: 03/06/2019  #
+# FINAL DATA POINT: 17/09/2019  #
+#                               #
+#################################
+
+
+setwd("C:/Users/ROMEST/Downloads/Performance/")
+
+options(scipen=999)
+
+# read excel file
+library(readxl)
+# split column from excel import
+library("tidyr")
+
+
+################################
+#                              #
+#     PERFORMANCE CARRELLO     #
+#                              #
+################################
+
+
+ricerca_performance_excel <- read_excel("ricerca_nodo1.xlsx")
+data <- as.data.frame(ricerca_performance_excel)
+
+# separate column
+data <- separate(data, time, into = c("empty", "time"), sep = " (?=[^ ]+$)")
+data <- data[,-2]
+names(data)[4] <- "avg_response"
+names(data)[1] <- "date"
+# add column for unique ID
+data$id <- seq.int(nrow(data))
+# remove comma from number
+data$avg_response <- as.numeric(gsub(",","", data$avg_response))
+# add column for month extraction
+data$new <- data$date
+# split new column for month extraction
+data <- separate(data, new, into = c("year", "month", "day"), sep = "-")
+
+
+# check for visible outliers
+plot(data$date, data$avg_response, main = "Ricerca e navigazione risultati - Nodo 1",
+     xlab = "Data", ylab = "Tempo risposta medio (3 ore), in millisecondi")
+
+# extract outliers rows for investivation
+outliers <- data[which(data$avg_response > 200000),]
+final_data <- data[-which(data$avg_response > 200000), ]
+
+# create subset data for each month
+june <- subset(final_data, month == "06")
+july <- subset(final_data, month == "07")
+august <- subset(final_data, month == "08")
+september <- subset(final_data, month == "09")
+
+# create variables for custom X axis (lid is where to show the mark, lab are the labels)
+lid <- c(1, 218, 466, 714)
+lab <- c("Giu", "Lug", "Ago", "Set")
+
+
+
+#plot(final_data$date, final_data$avg_response, type = "l", main = "Performance Catalogo",
+#    xlab = "Data", ylab = "Tempo risposta medio (3 ore), in millisecondi")
+
+plot(final_data$id, final_data$avg_response, type = "l", main = "Ricerca e navigazione risultati - Nodo 1",
+     xlab = "Data", ylab = "Tempo risposta medio (3 ore), in millisecondi", xaxt = "n")
+axis(side = 1, at = lid, labels = lab)
+
+# check situation before IT fix (june/july) vs after IT fix (august/september)
+# create data frame
+before <- rbind(june, july)
+after <- rbind(august, september)
+
+
+# TIME OF THE DAY
+# 10 SECONDS
+# BEFORE
+# extract rows where avg response > 10 sec
+b10s <- before[which(before$avg_response > 10000), ]
+# show distribution
+table(b10s$time)
+# AFTER
+# extract rows where avg response > 10 sec
+a10s <- after[which(after$avg_response > 10000), ]
+# show distribution
+table(a10s$time)
+
+# 5 SECONDS
+# BEFORE
+# extract rows where avg response > 5 sec
+b5s <- before[which(before$avg_response > 5000), ]
+# show distribution
+table(b5s$time)
+# AFTER
+# extract rows where avg response > 5 sec
+a5s <- after[which(after$avg_response > 5000), ]
+# show distribution
+table(a5s$time)
+
+# 2.5 SECONDS
+# BEFORE
+# extract rows where avg response > 2.5 sec
+b2.5s <- before[which(before$avg_response > 2500), ]
+# show distribution
+table(b2.5s$time)
+nrow(before[which(before$avg_response > 2500), ])
+# AFTER
+# extract rows where avg response > 2.5 sec
+a2.5s <- after[which(after$avg_response > 2500), ]
+# show distribution
+table(a2.5s$time)
+nrow(after[which(after$avg_response > 2500), ])
+
+# 1.5 SECONDS
+# BEFORE
+# extract rows where avg response > 1.5 sec
+b1.5s <- before[which(before$avg_response > 1500), ]
+# show distribution
+table(b1.5s$time)
+nrow(before[which(before$avg_response > 1500), ])
+# AFTER
+# extract rows where avg response > 1.5 sec
+a1.5s <- after[which(after$avg_response > 1500), ]
+# show distribution
+table(a1.5s$time)
+nrow(after[which(after$avg_response > 1500), ])
+
+
+
+
+sum(table(b1.5s$time))/nrow(before)
+sum(table(a1.5s$time))/nrow(after)
